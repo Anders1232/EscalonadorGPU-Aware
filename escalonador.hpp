41463e629c4cf071505620a9f7185c6dc9b4708d
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-04-15 12:35:49 -0300

    Melhorado a interface do escalonador

--- escalonador.hpp
+++ escalonador.hpp
@@ -1,14 +1,12 @@
 #include <cstdint>
 #include <vector>
+#include <unordered_map>
+#include <string>
 
 using std::vector;
-
-struct TipoJob
-{
-	uint64_t id;
-	(double) (*comoEscalaNaCPU)(uint64_t tamanhoDaEntrada);
-	(double) (*comoEscalaNaGPU)(uint64_t tamanhoDaEntrada);
-};
+using std::unordered_map;
+using std::string;
+//using std::
 
 struct Job
 {
@@ -26,7 +24,36 @@ class Escalonador
 {
 	public:
 		void AdicionarJob(Job);
-		void JobTerminado(uint64_t jobID);
+		void JobTerminado(uint64_t jobID, double duracao);
+		void AdicionarJob(uint64_t id, TipoJob dados);
 	private:
-		vector<TipoJob> tipoJob;
+		class TipoJob;
+		unordered_map<uint64_t, TipoJob> tabelaTipoJob;// a chave desse hash será o id do tipo de Job
+		vector<Job> listaJobsCPU;
+		vector<Job> listaJobsGPU;
+		vector<Job> jobsBloqueadosCPU;
+		vector<Job> jobsBloqueadosGPU;
 };
+
+class Escalonador::TipoJob
+{
+	public:
+		uint64_t ObterPontuacao(uint64_t tamanhoDaEntrada) const;
+		uint64_t EnviarFeedback(uint64_t tamanhoDaEntrada, double duracao);
+		string ObterNome(void) const;
+		Escalonador::TipoJob
+		(
+			string nome,
+			uint64_t pontuacaoEmCPU,
+			uint64_t pontuacaoEmGPU,
+			(uint64_t) (*comoEscalaNaCPU)(uint64_t tamanhoDaEntrada, uint64_t pontuacaoEmCPU),
+			(uint64_t) (*comoEscalaNaGPU)(uint64_t tamanhoDaEntrada, uint64_t pontuacaoEmGPU)//será que é necessário métodos para informar como o feddback deve alterar a pontuação??
+		);
+	private:
+		string nome;
+		uint64_t pontuacaoEmCPU;
+		uint64_t pontuacaoEmGPU;
+		(uint64_t) (*comoEscalaNaCPU)(uint64_t tamanhoDaEntrada, uint64_t pontuacaoEmCPU);
+		(uint64_t) (*comoEscalaNaGPU)(uint64_t tamanhoDaEntrada, uint64_t pontuacaoEmGPU);
+};
+
