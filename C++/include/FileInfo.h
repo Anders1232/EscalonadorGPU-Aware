29d5510bde61f2f9d98f36cffde9cb604cc368c8
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-09-27 00:21:13 -0300

    Adicionado fileInfo

---
+++ C++/include/FileInfo.h
@@ -0,0 +1,19 @@
+#ifndef FILEINFO_H
+#define FILEINFO_H
+
+#include <string>
+#include <stdint.h>
+#include "Serializable.h"
+
+class FileInfo: Serializable<FileInfo>{
+	public:
+		FileInfo(void):bucket(""){}
+		std::string id;
+		std::string name;
+		int64_t size;
+		int64_t userId;
+		std::string uploadTimestamp, hash, bucket;
+//		unsigned char[] payload;
+}
+
+#endif // FILEINFO_H
